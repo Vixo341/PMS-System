dddc64ba6e5c194218760ba4e1252f2de8977404
Vixo341 <malpa341@gmail.com>
2019-10-29 20:27:07 +0100

    backend

---
+++ Backend/src/main/java/com/example/pmstool/repositories/BacklogRepository.java
@@ -0,0 +1,13 @@
+package com.example.pmstool.repositories;
+
+
+
+import com.example.pmstool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
---
+++ Backend/src/test/java/com/example/pmstool/PmstoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmstool;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmstoolApplicationTests {
+
+   // @Test
+    void contextLoads() {
+    }
+
+}
