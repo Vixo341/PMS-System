401e5f101c216941e32cb004817538828f258546
Vixo341 <malpa341@gmail.com>
2019-11-11 13:38:45 +0100

    user registration

--- Backend/src/main/java/com/example/pmstool/services/UserService.java
+++ Backend/src/main/java/com/example/pmstool/services/UserService.java
@@ -1,8 +1,11 @@
 package com.example.pmstool.services;
 
 
+import com.example.pmstool.domain.User;
+import com.example.pmstool.exceptions.UsernameAlreadyExistsException;
 import com.example.pmstool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +14,20 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser (User newUser){
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            newUser.setUsername(newUser.getUsername());
+            newUser.setConfirmPassword("");
+            return userRepository.save(newUser);
+
+        }catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
+    }
 
 
 }
